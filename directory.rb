74e65ecce2179325d2b149571c1868b0ae842b83
Heli Sivunen <heli@mooncat.tv>
2019-09-09 14:29:39 +0100

    adds option to choose cohort. Centers text.

--- directory.rb
+++ directory.rb
@@ -1,13 +1,44 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+#added '.center'
+
+def input_students
+  puts "Please enter the name of the student"
+    puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+
+    while !name.empty? do
+  puts "Please enter the cohort month"
+  cohort = gets.chomp
+      # add the student hash to the array
+  students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
+    puts "Please enter the name of the student"
+    puts "To finish, just hit return twice"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
+end
+def print(students)
+    students.each_with_index do |student, i|
+    puts "#{i+1}: #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+   end
+  end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students".center(50)
+end
+
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
