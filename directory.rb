084aa8a20f7b1f049ad59b728b67aa8ee33e5801
Heli Sivunen <heli@mooncat.tv>
2019-09-11 12:08:44 +0100

    Saves to file (csv).

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# refactoring
+# saving to file (csv)
 
 @students = [] # an empty array accessible to all methods
 
@@ -47,6 +47,7 @@ end
 def print_menu
   puts "1. Input the students."
   puts "2. Show the students."
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -61,6 +62,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will terminate programme
   else
@@ -86,6 +89,17 @@ def print_footer
   if @students.count > 0
     puts "Overall, we have #{@students.count} great students".center(50)
   end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each.do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
