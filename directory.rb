9b3ebb7075af2aa384675c1639f9ac562ecc7f04
Heli Sivunen <heli@mooncat.tv>
2019-09-11 10:48:26 +0100

    Adds interactive menu.

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# changing it so that it only prints the list if there's at least one entry
+# adds interactive manu at the bottom
 
 def input_students
   puts "Please enter the name of the student"
@@ -58,7 +58,28 @@ def print_footer(students)
 end
 end
 
-students = input_students
-#nothing happens until we call the methods
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students."
+    puts "2. Show the students."
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+  #3. do what the user has asked
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print(students)
+    print_footer(students)
+  when "9"
+    exit # this will terminate programme
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+end
+
+interactive_menu
